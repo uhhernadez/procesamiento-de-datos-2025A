cf43d5ec3139df758ab9d56281e2f0d70bd8c7e6
Uriel Haile <perenganito@gmail.com>
2025-02-21 15:28:40 -0600

    Codigo de color

--- 2025_02_21_svg_dibujar/index.js
+++ 2025_02_21_svg_dibujar/index.js
@@ -11,3 +11,16 @@ line.attr("y1", 0);
 line.attr("x2", 100);
 line.attr("y2", 100);
 line.attr("style", `stroke:rgb(${r},0,0 );stroke-width:2`);
+
+const drawLine = (x1, y1, x2, y2, r, g, b) => {
+  svg.append("line")
+  .attr("x1",x1)
+  .attr("y1",y1)
+  .attr("x2",x2)
+  .attr("y2",y2)
+  .attr("style",`stroke:rgb(${r},${g},${b} );stroke-width:2`) 
+}
+
+for (let i = 0; i < 10; i++) {
+  drawLine(0,10*i,100, 10*i, 128, 0, 0);  
+}
