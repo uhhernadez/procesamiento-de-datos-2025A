da874268c124bf45ae963a2611a1c1a72df41183
Uriel Haile <perenganito@gmail.com>
2025-02-21 14:13:47 -0600

    Cambios en el dibujo

--- 2025_02_18_svg_dibujar/index.js
+++ 2025_02_18_svg_dibujar/index.js
@@ -1,2 +1,11 @@
 import {select} from "d3";
 const svg = select("#app").append("svg");
+svg.attr("width", window.innerWidth);
+svg.attr("height", window.innerHeight);
+
+const rect = svg.append("rect");
+rect.attr("x", window.innerWidth/2.0 );
+rect.attr("y", 20);
+rect.attr("width", 30);
+rect.attr("height",50);
+rect.attr("fill","blue");
